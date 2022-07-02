7273abab281121a5f38f14d9d5ebae3b4f376831
khmuhib <khmuhib2013@gmail.com>
2022-07-02 11:36:34 +0600

    First Time Upload

---
+++ vue/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+import {mapState} from 'vuex'
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+    computed: {
+        ...mapState(["user"])
+    },
+    components: { HelloWorld }
+}
+</script>
+
+<template>
+
+
+<div>
+  <HelloWorld />
+</div>
+
+
+</template>
+
+<style>
+pre {
+  font-size: 1.5em;
+}
+</style>
